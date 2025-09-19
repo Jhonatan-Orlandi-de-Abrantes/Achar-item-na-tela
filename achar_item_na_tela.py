a30686154f0c494c6288c7ce0603e0cdf17b9bdb
Jhonatan Orlandi de Abrantes <194443620+Jhonatan-Orlandi-de-Abrantes@users.noreply.github.com>
2025-09-19 11:20:06 -0300

    Update achar_item_na_tela.py

--- achar_item_na_tela.py
+++ achar_item_na_tela.py
@@ -1,16 +1,39 @@
-import pyautogui, time
+import pyautogui, time, keyboard as kb
 
-time.sleep(0.5)
+key = 'F8' # Tecla que ativa/desativa o script
+confirm = False
 
-while True: # Só irá parar de procurar até o item ser encontrado
-    time.sleep(0.5)
-    try: # Verificar se a imagem foi localizada
-        pos = pyautogui.locateOnScreen('item.png', confidence=0.9) # Localizar o item e definir a precisão (O quão perto ele irá achar a posição), necessário ter imagem com o nome "item.png"
-        if pos: # Se sim
-            print(f'Item encontrado. Posição: {pos}')
-            pyautogui.click(pos)
-            break
-        else: # Se não
-            print('Item não encontrado! Tentando novamente...')
-    except Exception as excecao: # Se houver uma exceção enquanto o try estiver rodando, irá parar o processo e mostrar qual foi a exceção
-        print(f'Ocorreu um erro: {excecao}')
+def toggle(e): # Função "toggle", ou seja, ativa e desativa o processo com um clique
+    global confirm
+    confirm = not confirm
+    print(f'Condicao = {confirm}')
+
+def search():
+    global confirm
+    while True:
+        if confirm:
+            while confirm:
+                time.sleep(0.5)
+                try:
+                    # Verifica primeiro se a imagem que deve ser evitada está na tela
+                    evitar = pyautogui.locateOnScreen('evitar_este_item.png', confidence=0.7)
+                    if evitar:
+                        print('Imagem para EVITAR encontrada. Ignorando tentativa de clique...')
+                        continue # Pula para a próxima iteração sem procurar o item
+                    
+                    # Agora verifica a imagem do item, já que a de evitar não foi encontrada
+                    pos = pyautogui.locateOnScreen('item.png', confidence=0.7)
+                    if pos:
+                        print(f'Item encontrado. Posição: {pos}')
+                        pyautogui.click(pos)
+                    else:
+                        print('Item não encontrado! Tentando novamente...')
+                except Exception as e:
+                    print(f'Erro ao procurar as imagens: {e}')
+        else:
+            time.sleep(0.1) # Evitar o uso excessivo de CPU quando inativo
+
+# Associa a tecla para ligar/desligar á função desejada
+kb.on_press_key(key, toggle)
+# Inicia a busca
+search()
